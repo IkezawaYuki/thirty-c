8dca779b190a25122e5c6717e6b422078d6048c2
yikezawa <y.ikezawa93@gmail.com>
2021-12-07 22:07:01 +0900

    first commit

---
+++ test/models/todo_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class TodoTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+
+  test "sleep 1" do
+    sleep 3
+    assert true
+  end
+
+  test "sleep2" do
+    sleep 3
+    assert true
+  end
+    
+  test "sleep3" do
+    sleep 3
+    assert true
+  end
+    
+  test "sleep4" do
+    sleep 3
+    assert true
+  end
+    
+end
